f04870ad3bd0bdde3be022c38ee74ab199366363
rodcantil <rodolfo.cantillana@gmail.com>
2024-09-29 00:58:34 -0300

    1ero

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def index
+    @users = User.all
+  end
+  def create
+    User.create(name: params[:name], age:params[:age], email:params[:email])
+  end
+end
